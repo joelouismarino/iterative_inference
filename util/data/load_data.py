changed the gradient update form, fixed imagenet loading, added covariance plots

util/data/load_data.py
@@ -9,14 +9,31 @@ from scipy.io import loadmat
 
 from load_torch_data import load_torch_data
 
-# todo: add label names to omniglot, imagenet
-# todo: add labels to static binarized MNIST, imagenet, omniglot
+# todo: add label names to omniglot
+# todo: add labels to static binarized MNIST, omniglot
 
 
 @load_torch_data
 def load_data(dataset, data_path):
 
-    """Loads and downloads a variety of benchmark image datasets."""
+    """
+    Downloads and loads a variety of benchmark image datasets.
+    Note that some datasets do not have labels and label names.
+
+    # Arguments
+        dataset: a string from one of the datasets supported below
+        data_path: a path string to the location of the datasets
+
+    # Returns
+        (train, val): for small datasets, these are data tensors of
+                      size  N x H x W x C. for larger datasets, these
+                      are paths to the image directories
+        (train_labels, val_labels): lists of label indices for the images.
+                                    note that some datasets do not have labels.
+        label_names: lists of (human readable) string names for the
+                     label indices. note that some datasets do not have
+                     label names.
+    """
 
     assert os.path.exists(data_path), 'Data path not found. Please specify a valid path.'
 
@@ -216,6 +233,13 @@ def load_data(dataset, data_path):
             tar.extractall(os.path.join(data_path, 'imagenet_32'))
             tar.close()
             os.remove(os.path.join(data_path, 'imagenet_32', 'train_32x32.tar'))
+            print 'Moving images into directory...'
+            os.makedirs(os.path.join(data_path, 'imagenet_32', 'train_32x32', 'images'))
+            for _, _, files in os.walk(os.path.join(data_path, 'imagenet_32', 'train_32x32')):
+                root = os.path.join(data_path, 'imagenet_32', 'train_32x32')
+                for f in files:
+                    if os.path.exists(os.path.join(root, f)):
+                        os.rename(os.path.join(root, f), os.path.join(root, 'images', f))
         train = os.path.join(data_path, 'imagenet_32', 'train_32x32')
 
         if not os.path.exists(os.path.join(data_path, 'imagenet_32', 'valid_32x32')):
@@ -226,6 +250,13 @@ def load_data(dataset, data_path):
             tar.extractall(os.path.join(data_path, 'imagenet_32'))
             tar.close()
             os.remove(os.path.join(data_path, 'imagenet_32', 'valid_32x32.tar'))
+            print 'Moving images into directory...'
+            os.makedirs(os.path.join(data_path, 'imagenet_32', 'valid_32x32', 'images'))
+            for _, _, files in os.walk(os.path.join(data_path, 'imagenet_32', 'valid_32x32')):
+                root = os.path.join(data_path, 'imagenet_32', 'valid_32x32')
+                for f in files:
+                    if os.path.exists(os.path.join(root, f)):
+                        os.rename(os.path.join(root, f), os.path.join(root, 'images', f))
         val = os.path.join(data_path, 'imagenet_32', 'valid_32x32')
 
     elif dataset == 'imagenet_64':
@@ -239,6 +270,13 @@ def load_data(dataset, data_path):
             tar.extractall(os.path.join(data_path, 'imagenet_64'))
             tar.close()
             os.remove(os.path.join(data_path, 'imagenet_64', 'train_64x64.tar'))
+            print 'Moving images into directory...'
+            os.makedirs(os.path.join(data_path, 'imagenet_64', 'train_64x64', 'images'))
+            for _, _, files in os.walk(os.path.join(data_path, 'imagenet_64', 'train_64x64')):
+                root = os.path.join(data_path, 'imagenet_64', 'train_64x64')
+                for f in files:
+                    if os.path.exists(os.path.join(root, f)):
+                        os.rename(os.path.join(root, f), os.path.join(root, 'images', f))
         train = os.path.join(data_path, 'imagenet_64', 'train_64x64')
 
         if not os.path.exists(os.path.join(data_path, 'imagenet_64', 'valid_64x64')):
@@ -249,6 +287,13 @@ def load_data(dataset, data_path):
             tar.extractall(os.path.join(data_path, 'imagenet_64'))
             tar.close()
             os.remove(os.path.join(data_path, 'imagenet_64', 'valid_64x64.tar'))
+            print 'Moving images into directory...'
+            os.makedirs(os.path.join(data_path, 'imagenet_64', 'valid_64x64', 'images'))
+            for _, _, files in os.walk(os.path.join(data_path, 'imagenet_64', 'valid_64x64')):
+                root = os.path.join(data_path, 'imagenet_64', 'valid_64x64')
+                for f in files:
+                    if os.path.exists(os.path.join(root, f)):
+                        os.rename(os.path.join(root, f), os.path.join(root, 'images', f))
         val = os.path.join(data_path, 'imagenet_64', 'valid_64x64')
 
     else:

util/logs.py
@@ -59,7 +59,7 @@ def log_vis(func):
 
     def log_func(model, train_config, data_loader, epoch, vis=False, eval=False):
         output = func(model, train_config, data_loader, vis=vis, eval=eval)
-        total_elbo, total_cond_log_like, total_kl, total_labels, total_recon, total_posterior, total_prior, samples = output
+        total_elbo, total_cond_log_like, total_kl, total_log_like, total_labels, total_recon, total_posterior, total_prior, samples = output
         update_metric(os.path.join(log_path, 'metrics', 'val_elbo.p'), (epoch, np.mean(total_elbo[:, -1], axis=0)))
         update_metric(os.path.join(log_path, 'metrics', 'val_cond_log_like.p'), (epoch, np.mean(total_cond_log_like[:, -1], axis=0)))
         for level in range(len(model.levels)):
@@ -83,6 +83,10 @@ def log_vis(func):
 
             samples = samples.reshape([batch_size]+data_shape)
             pickle.dump(samples, open(os.path.join(epoch_path, 'samples.p'), 'w'))
+
+        if eval:
+            pass
+            # todo: save log likelihood estimate
 
         return output
 

util/plotting.py
@@ -222,6 +222,16 @@ def plot_errors_over_iterations(total_recon, data, epoch):
     pass
 
 
+def plot_latent_covariance_martrix(posterior_mean, epoch, level):
+    """Plot the posterior covariance matrix for each latent level."""
+    global vis
+    # flatten and center posterior mean, compute covariance matrix, plot
+    posterior_mean = posterior_mean.reshape((-1, np.prod(posterior_mean.shape[1:])))
+    posterior_mean_centered = posterior_mean - np.mean(posterior_mean, axis=0)
+    covariance = np.dot(posterior_mean_centered.T, posterior_mean_centered) / posterior_mean_centered.shape[0]
+    vis.heatmap(covariance, opts=dict(title='Posterior Covariance, Epoch ' + str(epoch) + ', Level ' + str(level)))
+
+
 def plot_train(func):
     """Wrapper around training function to plot the outputs in corresponding visdom windows."""
     def plotting_func(model, train_config, data_loader, epoch, handle_dict, optimizers):
@@ -264,10 +274,13 @@ def plot_model_vis(func):
             plot_images(total_recon[:batch_size, 1].reshape([batch_size]+data_shape), caption='Reconstructions, Epoch ' + str(epoch))
             plot_images(samples.reshape([batch_size]+data_shape), caption='Samples, Epoch ' + str(epoch))
 
-            # plot t-sne for each level's posterior
-            for level in range(len(model.levels)):
+            # plot t-sne for each level's posterior (at first inference iteration)
+            for level in range(len(total_posterior)):
                 plot_tsne(total_posterior[level][:, 1, 0], 1 + total_labels, title='T-SNE Posterior Mean, Epoch ' + str(epoch) + ', Level ' + str(level), legend=label_names)
 
+            # plot the covariance matrix for each level's posterior (at first inference iteration)
+            for level in range(len(total_posterior)):
+                plot_latent_covariance_martrix(total_posterior[level][:, 1, 0], epoch, level)
 
             if train_config['n_iterations'] > 1:
                 # plot ELBO, reconstruction loss, KL divergence over inference iterations